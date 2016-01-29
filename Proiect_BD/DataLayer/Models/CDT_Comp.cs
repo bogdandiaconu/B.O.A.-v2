34de3e4935314a405d545a430c15bac7bf1aa481
MarianCeap <ceaparu.marian@gmail.com>
2016-01-29 23:49:01 +0200

    get_CDT_Comp v2

--- Proiect_BD/DataLayer/Models/CDT_Comp.cs
+++ Proiect_BD/DataLayer/Models/CDT_Comp.cs
@@ -4,6 +4,14 @@ using System.Linq;
 
 namespace DataLayer.Models
 {
+    public partial class newCDT_Comp
+    {
+        public int ID_Comandant { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public int ID_Grad { get; set; }
+        public string Grad { get; set; }
+    }
     public partial class CDT_Comp
     {
         public CDT_Comp()
@@ -17,11 +25,19 @@ namespace DataLayer.Models
         public int ID_Grad { get; set; }
         public virtual Grade Grade { get; set; }
         public virtual ICollection<Companie> Companies { get; set; }
-        public static IQueryable<Models.CDT_Comp> get_CDT_comp()
+        public static IQueryable<Models.newCDT_Comp> get_CDT_comp()
         {
             var bd = new DataLayer.Models.ProiectBDContext();
             var query = from b in bd.CDT_Comp
-                        select b;
+                        join g in bd.Grades on b.ID_Grad equals g.ID_Grad
+                        select new newCDT_Comp
+                        {
+                            ID_Comandant = b.ID_Comandant,
+                            Nume = b.Nume,
+                            Prenume = b.Prenume,
+                            ID_Grad = b.ID_Grad,
+                            Grad = g.Nume
+                        };
             return query;
         }
 
