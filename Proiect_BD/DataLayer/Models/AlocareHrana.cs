482fe4fee734a6dc2098c86cbc51b61388e57ead
MarianCeap <ceaparu.marian@gmail.com>
2016-01-29 02:02:38 +0200

    insert update delete AlocareHrana

--- Proiect_BD/DataLayer/Models/AlocareHrana.cs
+++ Proiect_BD/DataLayer/Models/AlocareHrana.cs
@@ -20,5 +20,49 @@ namespace DataLayer.Models
                         select b;
             return query;
         }
+
+        public static void insert_AlocareHrana(int student, int pret, int admin, int counter)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+            AlocareHrana aloc = new AlocareHrana
+            {
+                ID_Student = student,
+                ID_Pret = pret,
+                ID_Administrator = admin,
+                Counter = counter
+            };
+
+            bd.AlocareHranas.Add(aloc);
+            bd.Entry(aloc).State = System.Data.Entity.EntityState.Added;
+            bd.SaveChanges();
+        }
+
+        public static void delete_AlocareHrana(int student, int pret)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+            var aloc = (from b in bd.AlocareHranas
+                        where b.ID_Student == student &&
+                              b.ID_Pret == pret
+                        select b).First();
+
+            bd.AlocareHranas.Remove(aloc);
+            bd.Entry(aloc).State = System.Data.Entity.EntityState.Deleted;
+            bd.SaveChanges();
+        }
+
+        public static void update_AlocareHrana(int student, int pret, int admin, int counter)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+            var aloc = (from b in bd.AlocareHranas
+                        where b.ID_Student == student &&
+                              b.ID_Pret == pret
+                        select b).First();
+
+            aloc.ID_Administrator = admin;
+            aloc.Counter = counter;
+
+            bd.Entry(query).State = System.Data.Entity.EntityState.Modified;
+            bd.SaveChanges();
+        }
     }
 }
