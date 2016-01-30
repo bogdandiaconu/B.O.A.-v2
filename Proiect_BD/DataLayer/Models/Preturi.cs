023e0e2bb0c50c1dc8da347a0c0896f808729ae0
MarianCeap <ceaparu.marian@gmail.com>
2016-01-30 02:11:06 +0200

    Preturi

--- Proiect_BD/DataLayer/Models/Preturi.cs
+++ Proiect_BD/DataLayer/Models/Preturi.cs
@@ -21,7 +21,52 @@ namespace DataLayer.Models
             var bd = new DataLayer.Models.ProiectBDContext();
             var query = from b in bd.Preturis
                         select b;
+
+            var x = query.ToList();
             return query;
+        }
+
+        public static void insert_Preturi(string nume, double pret, DateTime data)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+
+            Preturi pr = new Preturi
+            {
+                Nume = nume,
+                Pret = pret,
+                Data = data
+            };
+
+            bd.Preturis.Add(pr);
+            bd.Entry(pr).State = System.Data.Entity.EntityState.Added;
+            bd.SaveChanges();
+        }
+
+        public static void delete_Preturi(int id)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+            var pr = (from b in bd.Preturis
+                      where b.ID_Pret == id
+                      select b).First();
+
+            bd.Preturis.Remove(pr);
+            bd.Entry(pr).State = System.Data.Entity.EntityState.Deleted;
+            bd.SaveChanges();
+        }
+
+        public static void update_Preturi(int id, string nume, double pret, DateTime data)
+        {
+            var bd = new DataLayer.Models.ProiectBDContext();
+            var pr = (from b in bd.Preturis
+                      where b.ID_Pret == id
+                      select b).First();
+
+            pr.Nume = nume;
+            pr.Pret = pret;
+            pr.Data = data;
+
+            bd.Entry(pr).State = System.Data.Entity.EntityState.Modified;
+            bd.SaveChanges();
         }
     }
 }
